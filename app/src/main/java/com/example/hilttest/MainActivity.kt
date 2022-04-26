458c09f1c159e73a1842cf169de024b3137f2c03
logoped583st <logoped583st@mail.ru>
2022-04-26 16:35:38 +0300

    init

---
+++ app/src/main/java/com/example/hilttest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hilttest
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import com.google.android.material.button.MaterialButtonToggleGroup
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
